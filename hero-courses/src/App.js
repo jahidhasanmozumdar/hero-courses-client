06cbfe0fe381adbf90608f09d85b7a327de95866
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2022-10-25 02:05:20 +0600

    project initialize

---
+++ hero-courses/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button className="btn">Button</button>
+  <button className="btn btn-primary border border-rounded">Button</button>
+  </>
+  );
+}
+
+export default App;
