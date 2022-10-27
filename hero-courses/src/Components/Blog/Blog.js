587e91ea1828db8aa0454271c5e89220c9e4e63b
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2022-10-27 22:47:57 +0600

    q2 added

--- hero-courses/src/Components/Blog/Blog.js
+++ hero-courses/src/Components/Blog/Blog.js
@@ -19,7 +19,7 @@ const Blog = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
       >
         <div className="collapse-title text-xl font-medium">
-          Focus me to see content
+        why use firebase?
         </div>
         <div className="collapse-content">
           <p>tabIndex={0} attribute is necessary to make the div focusable</p>
