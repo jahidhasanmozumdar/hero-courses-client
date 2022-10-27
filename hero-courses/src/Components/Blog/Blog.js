11e41883f9b8111f149c1478c8e3106ee956afd6
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2022-10-27 22:49:46 +0600

    q3  added

--- hero-courses/src/Components/Blog/Blog.js
+++ hero-courses/src/Components/Blog/Blog.js
@@ -30,10 +30,10 @@ const Blog = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
       >
         <div className="collapse-title text-xl font-medium">
-          Focus me to see content
+        how does work private route?
         </div>
         <div className="collapse-content">
-          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          
         </div>
       </div>
     </div>
