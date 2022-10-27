bb3f012bfb45307c440328e463fcaee744270404
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2022-10-27 22:52:11 +0600

    q4  added

--- hero-courses/src/Components/Blog/Blog.js
+++ hero-courses/src/Components/Blog/Blog.js
@@ -30,6 +30,17 @@ const Blog = () => {
         className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
       >
         <div className="collapse-title text-xl font-medium">
+        What is node? how work?
+        </div>
+        <div className="collapse-content">
+        The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated Logged in
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
         how does work private route?
         </div>
         <div className="collapse-content">
